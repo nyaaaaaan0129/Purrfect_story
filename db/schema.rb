c7e1df1d50f3d9766c98ab40fcbe4cf05fd09ad3
Hanaho Murata <nyaaaaaan0129@gmail.com>
2020-06-30 21:43:07 +0900

    try push and pull

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_085015) do
+ActiveRecord::Schema.define(version: 2020_06_30_111952) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
