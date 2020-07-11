53e99bad2ae289a02c06b19eff89758fc90c2cdc
vagrant <vagrant@localhost.localdomain>
2020-07-11 16:38:15 +0000

    sign_uo_font

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < ActionController::Base
    protected
 
   def configure_permitted_parameters
-    added_attrs1 = [ :email, :last_name, :first_name, :password, :password_confirmation ]
+    added_attrs1 = [ :email, :last_name, :first_name, :nickname, :password, :password_confirmation ]
     added_attrs2 = [ :email, :password, :password_confirmation ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs1
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs1
