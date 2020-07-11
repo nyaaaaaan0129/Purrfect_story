0d4509496cbd144935a6fa0fb584b5dccd52c4ba
vagrant <vagrant@localhost.localdomain>
2020-07-11 16:10:16 +0000

    signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_in_path_for(resource)
     case resource
     when Admin
@@ -25,5 +26,15 @@ class ApplicationController < ActionController::Base
     else
       new_member_session_path
       end
+  end
+
+   protected
+
+  def configure_permitted_parameters
+    added_attrs1 = [ :email, :last_name, :first_name, :password, :password_confirmation ]
+    added_attrs2 = [ :email, :password, :password_confirmation ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs1
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs1
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs2
   end
 end
