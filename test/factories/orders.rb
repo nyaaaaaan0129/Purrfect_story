8f177067d6f2a23569ab4dd5e39f913a81adba0d
vagrant <vagrant@localhost.localdomain>
2020-07-12 15:53:10 +0000

    addtest

---
+++ test/factories/orders.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :order do
+    postage {800}
+    payment_method {1}
+    address {"test"}
+    postal_code {"12345"}
+    name {"test"}
+    total_fee {3000}
+    order_status {0}
+    nickname {"test"}
+    member { FactoryBot.create(:member)}
+  end
+end
+
+
+    
