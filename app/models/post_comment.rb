46b2b02f7114a86e1238b460ab6885ed6499c8cd
vagrant <nyaaaaaan0129@gmail.com>
2020-07-21 11:01:29 +0000

    bichousei

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -4,5 +4,5 @@ class PostComment < ApplicationRecord
   belongs_to :member
   belongs_to :post
 
-  validates :comment, presence: true, length:{ maximum: 200, minimum: 20, message: '20文字以上200文字以内です'}
+  validates :comment, presence: true, length:{ maximum: 200, minimum: 10, message: '10文字以上200文字以内です'}
 end
